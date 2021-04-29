2baac8bea4358c7596f9943aaac2031d5f0d2b1d
Eddy <eddy147@gmail.com>
2021-04-29 18:21:49 +0200

    Saving to repo

---
+++ lib/mastery/core/quiz.ex
@@ -0,0 +1,58 @@
+defmodule Mastery.Core.Quiz do
+  alias Mastery.Core.{Template, Question, Response}
+
+  defstruct title: nil,
+            mastery: 3,
+            templates: %{},
+            used: [],
+            current_question: nil,
+            last_response: nil,
+            record: %{},
+            mastered: []
+
+  def new(fields) do
+    struct!(__MODULE__, fields)
+  end
+
+  def add_template(quiz, fields) do
+    template = Template.new(fields)
+
+    templates =
+      update_in(
+        quiz.templates,
+        [template.category],
+        &add_to_list_or_nil(&1, template)
+      )
+
+    %{quiz | templates: templates}
+  end
+
+  def select_question(%__MODULE__{templates: t}) when map_size(t) == 0, do: nil
+  def select_question(quiz) do
+    quiz
+    |> Map.put(:current_question, select_a_random_question(quiz))
+    |> move_tmeplate(:used)
+    |> reset_template_cycle()
+  end
+
+  defp pick_current_question(quiz) do
+    Map.put(
+      quiz,
+      :current_question,
+      select_a_random_question(quiz)
+    )
+  end
+
+  defp select_a_random_question(quiz) do
+    quiz.templates
+    |> Enum.random()
+    |> elem(1)
+    |> Enum.random()
+    |> Question.new()
+  end
+
+  defp add_to_list_or_nil(nil, template), do: [template]
+  defp add_to_list_or_nil(templates, template), do: [template | templates]
+
+  defp move_template
+end
